80cfd10709292ceca8cb0056b87a4f634a968de0
Uhirwa-melissa54/ <uhirwamelissa@gmail.com>
2025-11-02 19:11:21 +0200

    initial commit

---
+++ bluetooth_connect/bluetooth_connect.ino
@@ -0,0 +1,14 @@
+#include <SofwareSerial.h>
+SoftwareSerial bt(10,11);
+void setup(){
+  Serial.begin(38400);
+  bt.begin(38400);
+}
+void loop(){
+  if(Serial.available()){
+    bt.write(Serial.read());
+  }
+  if(bt.available()){
+    Serial.print(bt.readString())
+  }
+}

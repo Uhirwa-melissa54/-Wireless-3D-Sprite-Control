2fca1532358fd02b5f3535ce2d0b2ca740b83663
Uhirwa-melissa54/ <uhirwamelissa@gmail.com>
2025-11-02 19:14:21 +0200

    Imported well Software.h

--- bluetooth_connect/bluetooth_connect.ino
+++ bluetooth_connect/bluetooth_connect.ino
@@ -1,14 +1,17 @@
-#include <SofwareSerial.h>
-SoftwareSerial bt(10,11);
-void setup(){
+#include <SoftwareSerial.h>
+
+SoftwareSerial bt(10, 11);
+
+void setup() {
   Serial.begin(38400);
   bt.begin(38400);
 }
-void loop(){
-  if(Serial.available()){
+
+void loop() {
+  if (Serial.available()) {
     bt.write(Serial.read());
   }
-  if(bt.available()){
-    Serial.print(bt.readString())
+  if (bt.available()) {
+    Serial.print(bt.readString());
   }
 }
